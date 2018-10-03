b3684d110c20232da04a7b5437a85cfb0037bd3d
thiago guedes <tguedes6@hotmail.com>
2018-10-03 10:18:40 -0300

    criado pasta trash

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import { isAuthenticated } from './../auth';
+import { isAuthenticated } from './../shared/auth';
 
 const PrivateRoute = ({component: Component, ...rest}) => (
   <Route {...rest} render={props => (
